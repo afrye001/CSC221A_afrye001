187f0f5a0c46e225d3fee1c98b4da1bdde345a8a
afrye001 <afrye001@plattsburgh.edu>
2024-02-01 12:43:00 -0500

    added Class2.py

---
+++ Class2.py
@@ -0,0 +1,17 @@
+# type in name; store in var  
+# output name; append "hello" to {name}
+
+
+name = input("Type in your name: ")
+total = 0 
+
+if (name == "AJ"):
+    print(f"Granted. Hello {name}")
+else: 
+    print("Denied") 
+
+while total <= 100:
+    print("Less than")
+    break
+else:
+    print("Greater than")
